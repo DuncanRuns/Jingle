957c5df35e321bebf1764a9c791ab329f85afcea
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2024-09-07 23:54:06 -0300

    Add customization to Misc script

--- src/main/resources/defaultscripts/Misc.lua
+++ src/main/resources/defaultscripts/Misc.lua
@@ -17,9 +17,34 @@ function get_reset_key()
 end
 
 function can_run_reset()
-    return jingle.isInstanceActive() and
-            ((jingle.getInstanceState() == "INWORLD" and jingle.getInstanceInWorldState() == "UNPAUSED")
-                    or (jingle.getInstanceState() == "PREVIEWING"))
+    if not jingle.isInstanceActive() then
+        return false
+    end
+
+    state = jingle.getInstanceState()
+
+    if state == 'INWORLD' then
+        state = jingle.getInstanceInWorldState()
+        if state == 'UNPAUSED' then
+            return jingle.getCustomizable('iwu', 'true') == 'true'
+        end
+        if state == 'PAUSED' then
+            return jingle.getCustomizable('iwp', 'true') == 'true'
+        end
+        if state == 'GAMESCREENOPEN' then
+            return jingle.getCustomizable('iwgso', 'false') == 'true'
+        end
+    end
+
+    if state == 'TITLE' then
+        return jingle.getCustomizable('t', 'false') == 'true'
+    end
+
+    if state == 'PREVIEWING' then
+        return jingle.getCustomizable('p', 'false') == 'true'
+    end
+
+    return false
 end
 
 function run_safe_reset()
@@ -56,8 +81,19 @@ function run_start_coping()
     jingle.sendChatMessage("/gamemode spectator")
 end
 
+function customize()
+    jingle.addCustomizationMenuText("Allowed states for 'Safe Resets' and 'Reset Before 20s':")
+    jingle.addCustomizationMenuCheckBox("iwu", true, "In World, Unpaused")
+    jingle.addCustomizationMenuCheckBox("iwp", true, "In World, Paused")
+    jingle.addCustomizationMenuCheckBox("iwgso", false, "In World, Inventory/Chat Open")
+    jingle.addCustomizationMenuCheckBox("t", false, "Title Screen")
+    jingle.addCustomizationMenuCheckBox("p", false, "Previewing World")
+    jingle.showCustomizationMenu()
+end
+
 jingle.addHotkey("Clear Worlds", run_clear_worlds)
 jingle.listen("ENTER_WORLD", save_enter_world_time)
 jingle.addHotkey("Safe Reset", run_safe_reset)
 jingle.addHotkey("Reset Before 20s", run_reset_before_20s)
 jingle.addHotkey("Start Coping", run_start_coping)
+jingle.setCustomization(customize)
