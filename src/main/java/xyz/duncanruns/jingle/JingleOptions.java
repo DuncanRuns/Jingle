5d2a4e1627e05f9eb2d2cd9a45d47950273fd7e3
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2024-09-26 22:12:02 -0300

    add DEFAULTS

--- src/main/java/xyz/duncanruns/jingle/JingleOptions.java
+++ src/main/java/xyz/duncanruns/jingle/JingleOptions.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 public class JingleOptions {
     public static final Path OPTIONS_PATH = Jingle.FOLDER.resolve("options.json").toAbsolutePath();
+    public static final JingleOptions DEFAULTS = new JingleOptions();
 
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
