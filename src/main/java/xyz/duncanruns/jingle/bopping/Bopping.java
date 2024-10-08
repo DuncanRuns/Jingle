3db521f3997129423ceb939a968a99af82bb04ac
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2024-10-08 20:36:42 -0300

    Bopping improvements (x20 speed no way)

--- src/main/java/xyz/duncanruns/jingle/bopping/Bopping.java
+++ src/main/java/xyz/duncanruns/jingle/bopping/Bopping.java
@@ -3,14 +3,16 @@ package xyz.duncanruns.jingle.bopping;
 import org.apache.logging.log4j.Level;
 import xyz.duncanruns.jingle.Jingle;
 import xyz.duncanruns.jingle.gui.JingleGUI;
-import xyz.duncanruns.jingle.instance.OpenedInstance;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -24,7 +26,6 @@ public final class Bopping {
             synchronized (Bopping.class) {
                 JingleGUI.get().clearWorldsButton.setEnabled(false);
                 JingleGUI.get().clearWorldsFromAllButton.setEnabled(false);
-                AtomicInteger threadNum = new AtomicInteger(0);
                 Stream<Path> paths;
                 if (allSeen) {
                     paths = Jingle.options.seenPaths.keySet().stream()
@@ -32,26 +33,17 @@ public final class Bopping {
                 } else {
                     paths = Jingle.getMainInstance().map(instance -> Stream.of(instance.instancePath)).orElseGet(Stream::empty);
                 }
-                List<Thread> clearWorldThreads = paths
-                        .map(Path::toAbsolutePath)
+                paths.map(Path::toAbsolutePath)
                         .distinct()
+                        .parallel()
                         .filter(Files::isDirectory)
-                        .map(path -> new Thread(() -> {
+                        .forEach(path -> {
                             try {
                                 clearWorlds(path);
                             } catch (IOException e) {
                                 Jingle.logError("Failed to clear worlds:", e);
                             }
-                        }, "world-bopper-" + threadNum.addAndGet(1)))
-                        .collect(Collectors.toList());
-                clearWorldThreads.forEach(Thread::start);
-                clearWorldThreads.forEach(thread -> {
-                    try {
-                        thread.join();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
+                        });
                 synchronized (Jingle.class) {
                     JingleGUI.get().clearWorldsButton.setEnabled(Jingle.getMainInstance().isPresent());
                     JingleGUI.get().clearWorldsFromAllButton.setEnabled(true);
@@ -67,8 +59,11 @@ public final class Bopping {
         if (!Files.isDirectory(savesPath)) {
             return;
         }
+        Jingle.log(Level.INFO, "Finding worlds to delete for \"" + instancePath + "\"...");
         // Get all worlds that are allowed to be deleted
-        List<Path> worldsToRemove = Arrays.stream(Objects.requireNonNull(savesPath.toFile().list())) // Get all world names
+
+        List<Path> worldsToRemove = Files.list(savesPath) // Get all world paths
+                .parallel()
                 .map(savesPath::resolve) // Map to world paths
                 .filter(Bopping::shouldDelete) // Filter for only ones that should be deleted
                 .sorted(Comparator.comparing(value -> value.toFile().lastModified(), Comparator.reverseOrder())) // Sort by most recent first
@@ -76,32 +71,38 @@ public final class Bopping {
         // Remove the first 36 (or less) worlds
         worldsToRemove.subList(0, Math.min(36, worldsToRemove.size())).clear();
         // Actually delete stuff
-        int i = 0;
         int total = worldsToRemove.size();
         if (worldsToRemove.isEmpty()) {
             Jingle.log(Level.INFO, "No worlds to clear for \"" + instancePath + "\"");
             return;
         }
-        for (Path path : worldsToRemove) {
-            if (++i % 50 == 0) {
+        AtomicInteger cleared = new AtomicInteger();
+        worldsToRemove.parallelStream().forEach(path -> {
+            try {
+                Files.walk(path)
+                        .sorted(Comparator.reverseOrder())
+                        .map(Path::toFile)
+                        .forEach(File::delete);
+            } catch (IOException e) {
+                Jingle.logError("Failed to delete world \"" + path + "\".", e);
+            }
+            int i = cleared.incrementAndGet();
+            if (i % 50 == 0) {
                 Jingle.log(Level.INFO, "Clearing \"" + instancePath + "\": " + i + "/" + total);
             }
-            Files.walk(path)
-                    .sorted(Comparator.reverseOrder())
-                    .map(Path::toFile)
-                    .forEach(File::delete);
-        }
+        });
         Jingle.log(Level.INFO, "Cleared worlds for \"" + instancePath + "\"");
     }
 
     private static boolean shouldDelete(Path path) {
-        if ((!path.toFile().isDirectory()) || (path.resolve("Reset Safe.txt").toFile().isFile()) || !Files.isRegularFile(path.resolve("level.dat"))) {
-            return false;
-        }
+        if (!path.toFile().isDirectory() || path.resolve("Reset Safe.txt").toFile().isFile()) return false;
+
         String name = path.getFileName().toString();
-        if (name.startsWith("_")) {
-            return false;
-        }
-        return name.startsWith("New World") || name.startsWith("Benchmark Reset #") || name.contains("Speedrun #") || name.contains("Practice Seed") || name.contains("Seed Paster");
+        if (name.startsWith("Benchmark Reset #")) return true; // Sometimes benchmark might not have level.dat
+
+        if (!Files.isRegularFile(path.resolve("level.dat"))) return false;
+        if (name.startsWith("_")) return false;
+
+        return name.startsWith("New World") || name.contains("Speedrun #") || name.contains("Practice Seed") || name.contains("Seed Paster");
     }
 }
