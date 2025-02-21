41a1267cef4ed6b3de04b8533cf8344e34309f13
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2025-02-21 10:30:36 -0400

    Add PluginHotkeys.removeHotkeyAction(...)

--- src/main/java/xyz/duncanruns/jingle/plugin/PluginHotkeys.java
+++ src/main/java/xyz/duncanruns/jingle/plugin/PluginHotkeys.java
@@ -11,6 +11,10 @@ public final class PluginHotkeys {
     public static void addHotkeyAction(String name, Runnable runnable) {
         HOTKEYS_ACTIONS.put(name, runnable);
     }
+    
+    public static void removeHotkeyAction(String name) {
+        HOTKEYS_ACTIONS.remove(name);
+    }
 
     public static Set<String> getHotkeyActionNames() {
         return Collections.unmodifiableSet(HOTKEYS_ACTIONS.keySet());
