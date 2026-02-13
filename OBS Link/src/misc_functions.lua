8fc9e03a06062125bafea731aae733563d1a1fe5
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2026-02-13 08:00:09 -0400

    Remove the rest of obs link

--- OBS Link/src/misc_functions.lua
+++
@@ -1,8 +0,0 @@
-function split_string(input_string, split_char)
-    local out = {}
-    -- https://stackoverflow.com/questions/1426954/split-string-in-lua
-    for str in input_string.gmatch(input_string, "([^" .. split_char .. "]+)") do
-        table.insert(out, str)
-    end
-    return out
-end
